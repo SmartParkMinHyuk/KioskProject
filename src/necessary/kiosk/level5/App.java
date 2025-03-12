c02ada2f36553529ca4fea5aec0d9a537a37c7e0
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-03-12 13:40:13 +0900

    feat : Level 5 캡슐화 및 코드 수정

---
+++ src/necessary/kiosk/level5/App.java
@@ -0,0 +1,52 @@
+package necessary.kiosk.level5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 제목 : 키오스크 Level 5 구현.
+ * 날짜 : 2025/03/11
+ * 작성자 : 박민혁
+ * 개요 : 코딩스파르타 Spring 과정 3주차 LEVEL 5 과제 ( LEVEL 5/5 )
+ * 요구사항이 가지는 의도
+ * 'MenuItem`, `Menu` 그리고 `Kiosk` 클래스의 필드에 직접 접근하지 못하도록 설정합니다.
+ * Getter와 Setter 메서드를 사용해 데이터를 관리합니다.
+ * */
+public class App {
+
+    public static void main(String[] args) {
+        // 버거 데이터
+        List<MenuItem> burgers = new ArrayList<>();
+        burgers.add(new MenuItem("ShackBurger",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("SmokeShack",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("Cheeseburger",6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("Hamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
+        Menu burger = new Menu("Burgers", burgers);
+
+        // 음료 데이터
+        List<MenuItem> drinks = new ArrayList<>();
+        drinks.add(new MenuItem("Coke",5.0,"suger, water, suger"));
+        drinks.add(new MenuItem("Sprite",4.5,"suger, water, suger"));
+        drinks.add(new MenuItem("Beer",9.0,"suger, water, meal"));
+        drinks.add(new MenuItem("Wine",5.4,"grape, wawter, achol"));
+        Menu drink = new Menu("drinks", drinks);
+
+        // 디저트 데이터
+        List<MenuItem> Desserts = new ArrayList<>();
+        Desserts.add(new MenuItem("Cheese Cake",6.9,"milk, water, 숙성 120일"));
+        Desserts.add(new MenuItem("Tomato",8.9,"현지에서 막 따온 토마토"));
+        Desserts.add(new MenuItem("Banana",6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        Desserts.add(new MenuItem("Water",5.4,"경기도 광주 처맛골에서 떠온 미네랄 듬뿍 물"));
+        Menu Dessert = new Menu("Desserts", Desserts);
+
+        // kiosksr 객체 생성
+        List<Menu> kiosks = new ArrayList<>();
+        kiosks.add(burger);
+        kiosks.add(drink);
+        kiosks.add(Dessert);
+
+        Kiosk kiosk = new Kiosk(kiosks);
+
+        kiosk.start();
+    }
+}
---
+++ src/necessary/kiosk/level5/Kiosk.java
@@ -0,0 +1,139 @@
+package necessary.kiosk.level5;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+
+    // console 색 데이터
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+
+    private final List<Menu> mainMenuList;
+
+    public Kiosk(List<Menu> mainMenuList) {
+        this.mainMenuList = mainMenuList;
+    }
+
+    // 키오스크 순서를 제어
+    public void start() {
+
+        // 메인 메뉴 화면
+        while(true) {
+
+            int option1;
+
+            try {
+                printMenu();
+
+                option1 = inputOption();
+
+                if (option1 == 0) {
+                    return;
+                } else {
+                    selectMenu(option1);
+                }
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                continue;
+            }
+            // 서브 메뉴 아이템 화면
+            while(true) {
+
+                int option2;
+
+                try {
+                    printMenuItem(option1);
+
+                    option2 = inputOption();
+
+                    if (option2 == 0) {
+                        break;
+                    } else {
+                        selectMenuItem(option1, option2);
+                    }
+                } catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void printMenu() {
+        int index = 1;
+        for (Menu menu : mainMenuList) {
+            System.out.println(
+                    RED + (index) + "." + PURPLE + String.format("%-15s", menu.getMenuName()) + RESET);
+            index++;
+        }
+        System.out.println(RED + "0."+ RESET +"exit           | exit");
+    }
+
+    public int inputOption() {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("원하시는 번호를 입력해주세요 : ");
+            return sc.nextInt();
+        } catch (Exception e) {
+            sc.nextLine();
+            throw new IllegalArgumentException("잘못된 값입니다. 숫자를 입력하세요. 다시 입력하세요.");
+        }
+    }
+
+    public void selectMenu(int option1) {
+
+        // 기존코드
+        // menu.get(option1 - 1).getMenuName()
+
+        // 변경코드
+        // Menu selectedMenu = menu.get(option1 - 1);
+        // selectedMenu.getMenuName()
+
+        Menu selectedMenu = mainMenuList.get(option1 - 1);
+
+        if (option1 < 0 || option1 > mainMenuList.size()) {
+            throw new IndexOutOfBoundsException("잘못된 메뉴 번호입니다. 다시 입력하세요.");
+        } else {
+            System.out.println(
+                    selectedMenu.getMenuName() + "메뉴를 선택하셨습니다.");
+        }
+    }
+
+    public void printMenuItem(int option1) {
+        Menu selectedMenu = mainMenuList.get(option1 - 1);
+        selectedMenu.printMenuItem(selectedMenu.getMenuItem());
+    }
+
+    public void selectMenuItem(int option1, int option2) {
+
+        // 기존코드
+        // menu.get(option1 - 1).getMenuItem().get(option2 - 1).getName()
+
+        // 변경코드
+        // Menu selectedMenu = menu.get(option1 - 1); kiosk 객체  -> Menu 객체
+        // MenuItem selectedMenuItem = selectedMenu.getMenuItem().get(option2 - 1); Menu 객체 -> Menuitem 객체
+        // selectedMenuItem.getName();
+
+        Menu selectedMenu = mainMenuList.get(option1 - 1);
+        MenuItem selectedMenuItem = selectedMenu.getMenuItem().get(option2 - 1);
+
+        if (option2 < 0 || option2 > mainMenuList.get(option1 - 1).getMenuItem().size()) {
+            throw new IndexOutOfBoundsException("잘못된 아이템 번호입니다. 다시 입력하세요.");
+        } else {
+            System.out.println(
+                    selectedMenuItem.getName() + "아이템를 선택하셨습니다.");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
